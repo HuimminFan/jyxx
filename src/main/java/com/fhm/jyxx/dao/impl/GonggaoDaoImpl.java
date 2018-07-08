e0e016240649c14d581602398c6c3c5778cb15f6
tom_proglb <hello_glb@fhm.com>
2018-07-08 21:02:52 +0800

    First commit...

--- src/main/java/com/fhm/jyxx/dao/impl/GonggaoDaoImpl.java
+++ src/main/java/com/fhm/jyxx/dao/impl/GonggaoDaoImpl.java
@@ -1,32 +1,33 @@
 package com.fhm.jyxx.dao.impl;
 
 import com.fhm.jyxx.dao.BaseDao;
-import com.fhm.jyxx.domain.User;
+import com.fhm.jyxx.domain.Gonggao;
 import org.mybatis.spring.support.SqlSessionDaoSupport;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public class GonggaoDaoImpl extends SqlSessionDaoSupport implements BaseDao<User>{
-    public void insert(User user) {
-        getSqlSession().insert("t_user.insert",user);
+public class GonggaoDaoImpl extends SqlSessionDaoSupport implements BaseDao<Gonggao>{
+
+    public void insert(Gonggao gonggao) {
+        getSqlSession().insert("t_gonggao.insert",gonggao);
     }
 
-    public void delete(User user) {
-        getSqlSession().delete("t_user.delete",user);
+    public void delete(Gonggao gonggao) {
+        getSqlSession().delete("t_gonggao.delete",gonggao);
     }
 
     public void update(Integer id) {
-        getSqlSession().update("t_user.update",id);
+        getSqlSession().update("t_gonggao.update",id);
     }
 
-    public User selectOne(Integer id) {
-        return getSqlSession().selectOne("t_user.selectOne",id);
+    public Gonggao selectOne(Integer id) {
+        return getSqlSession().selectOne("t_gonggao.selectOne",id);
     }
 
-    public List<User> selectAll() {
-        return getSqlSession().selectList("t_user.selectAll");
+    public List<Gonggao> selectAll() {
+        return getSqlSession().selectOne("t_gonggao.selectAll");
     }
 
     public int selectCount() {
